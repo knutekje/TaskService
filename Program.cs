bd0e4ebb9725a9f4d0d99a5c797327a8b66c679d
knutekje <knutekjersem@gmail.com>
2024-12-19 10:17:33 +0100

    update on env vars

--- Program.cs
+++ Program.cs
@@ -21,11 +21,27 @@ builder.Host.UseSerilog();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton(sp =>
+/*builder.Services.AddSingleton(sp =>
 {
     var connectionString = "mongodb://debstar:27017"; // Adjust as needed
     var databaseName = "taskservice";
     return new MongoDbContext(connectionString, databaseName);
+});*/
+
+
+// Get MongoDB connection string from environment variables
+var mongoConnectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING");
+
+if (string.IsNullOrEmpty(mongoConnectionString))
+{
+    throw new InvalidOperationException("MongoDB connection string is not set in the environment variables.");
+}
+
+// Configure MongoDB
+builder.Services.AddSingleton<IMongoDatabase>(sp =>
+{
+    var mongoClient = new MongoClient(mongoConnectionString);
+    return mongoClient.GetDatabase("taskdb"); // Replace with your database name
 });
 
 
